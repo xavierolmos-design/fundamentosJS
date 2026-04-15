5fd2728325c494223752e491de4d6336aaa9d60b
Xavier olmos <xavierolmos@liceovvh.cl>
2026-04-15 08:10:08 -0400

    Waos

---
+++ 01_introduccion/1_hola_mundo/static/js/script.js
@@ -0,0 +1,11 @@
+console.log("Conexion exitosa con js externo");
+
+// 1) hola mundo con variable y const.
+const saludo = "Hola a todos, ";
+let destinatario = "Estudiantes, ";
+// Impresion de saludo concatenando con (+)
+console.log(saludo + "para los " + destinatario)
+// 2) saludar a una persona ingresada por teclado
+let nombre = prompt("ingrese su nombre: ")
+//Imprimir un nombre ingresado con prompt
+console.log("Hola," + nombre);
---
+++ 02_Tipos_de_datos/1,-Tipo_texto/static/js/script.js
@@ -0,0 +1,9 @@
+console.log("Conexion con JS correcta...")
+/*
+Concatenacion de texto con el signo +
+podemos unir texto y variables 
+*/
+const nombre = "Xavier"
+const apellido = "Olmos"
+//Unimos ambas constatantes con un texto extra.
+console.log("Hola, mi nombre es: " + nombre + " " + apellido)
