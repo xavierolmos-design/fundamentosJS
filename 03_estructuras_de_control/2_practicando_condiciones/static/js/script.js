79be2344c2c3df074c1df01342af0f73a50391f7
Xavier olmos <xavierolmos@liceovvh.cl>
2026-04-30 12:57:00 -0400

    f

--- 03_estructuras_de_control/2_practicando_condiciones/static/js/script.js
+++ 03_estructuras_de_control/2_practicando_condiciones/static/js/script.js
@@ -91,9 +91,14 @@ function ejercicio7() {
 // Resultado esperado en alert:
 // "Estudiante aprobado"
 
-function ejercicio8(){}
-
-
+function ejercicio8() {
+    let Nota = 6.5
+    if (Nota >= 5.0) 
+        alert(`Estudiante aprovado`)
+    else {
+        alert(`Estudiante desaprovado`)
+    }
+    };
 
 // EJERCICIO 9
 // Resultado esperado en alert:
@@ -118,4 +123,64 @@ function ejercicio10() {
     } else {
         alert(`El numero mayor es: ${num2}`)
     }
+}
+    function ejercicio11() {
+    let nombre = "Juan";
+    let nota = 6.5;
+    if (nota >= 6.0) {
+        alert(`Excelente rendimiento`);
+    } else if (nota >= 4.0) {
+        alert(`Estudiante aprobado`);
+    } else {
+        alert(`Estudiante reprobado`)
+    }
+}
+
+
+// EJERCICIO 12
+// Resultado esperado en alert:
+// `El nombre convertido es: DANIEL PÉREZ y tiene 12 caracteres`
+
+function ejercicio12() {
+    let nombre1 = "Daniel Perez";
+    alert(`El nombre convertido es: ${nombre1.toUpperCase()} tiene: ${nombre1.length} letras`)
+}
+
+
+// EJERCICIO 13
+// Resultado esperado en alert:
+// `El correo convertido es: ejemplo@gmail.com y el resultado es: correo válido`
+
+function ejercicio13() {
+let correo="ejemplo@gmail.com";
+let texto="correo valido";
+alert(`El correo convertido es: ${correo} y el resultado es: ${texto.toLowerCase()}`)
+}
+
+
+// EJERCICIO 14
+// Resultado esperado en alert:
+// `La frase "Estoy aprendiendo JavaScript" tiene 28 caracteres y corresponde a una frase larga`
+
+function ejercicio14() {
+let frase="Estoy aprendiendo ";
+if(frase){
+    alert(`la frase ${frase.toLowerCase()} tiene ${frase.length} caracteres y es una frase larga `);
+}else if (frase > 21){
+    alert(`la frase ${frase} tiene ${frase.length} caracteres y es frase mediana tiene entre 11 y 20 caracteres`);
+}else{
+    alert(`tiene  ${frase.length} y es frase corta tiene entre 10 o menos caracteres`)
+}
+}
+
+
+// EJERCICIO 15
+// Resultado esperado en alert:
+// `El producto TECLADO tiene un precio final de $45000`
+
+function ejercicio15() {
+let producto = "teclado"
+let precio = 50000
+let descuento = 0.1
+alert(`el producto ${producto.toLocaleUpperCase()} tiene un pecio final $${precio -(precio * descuento)}`)
 }
